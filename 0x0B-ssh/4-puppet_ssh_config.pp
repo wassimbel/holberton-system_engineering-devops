cce8b69c254446df8762f27602720f4c2e1f98b1
wassimbel <wassimbel.belhedi1@gmail.com>
2020-08-04 20:35:28 +0000

    puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # config ssh
 file_line { 'passwd auth':
   ensure => 'present',
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentification no'
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentification no'
 }
 
 file_line { 'private key path':
   ensure => 'present',
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentifyFile ~/holberton/ssh'
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/holberton/ssh'
 }
