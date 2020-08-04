6fb3c2881aac905def3e35323d5119b6f408c0df
wassimbel <wassimbel.belhedi1@gmail.com>
2020-08-04 20:55:08 +0000

    up

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 # config ssh
-
-file_line { 'remove passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentification no',
+file_line { 'passwd auth':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
 }
 
-file_line { 'add private key':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
+file_line { 'private key':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
 }
