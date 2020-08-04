8d16741f800a1ce37f84841dd48fb05991655c11
wassimbel <wassimbel.belhedi1@gmail.com>
2020-08-04 20:53:53 +0000

    up

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -3,11 +3,11 @@
 file_line { 'remove passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentification no'
+  line   => 'PasswordAuthentification no',
 }
 
 file_line { 'add private key':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton'
+  line   => 'IdentityFile ~/.ssh/holberton',
 }
