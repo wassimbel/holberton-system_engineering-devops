988e1fbe6f2f2d37007b3e74b814b947997cb07a
wassimbel <wassim.belhedi1@gmail.com>
2020-08-15 05:20:51 +0000

    new

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++
@@ -1,5 +0,0 @@
-# same as task 0
-exec {'header':
-command  => 'sudo apt-get update -y; sudo apt-get install nginx -y; sudo sed -i "15i \\\n\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf; service nginx restart',
-provider => shell,
-}
