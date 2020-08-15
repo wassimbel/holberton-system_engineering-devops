00f406441916068b1803e2c24b73f89e8348b2be
wassimbel <wassim.belhedi1@gmail.com>
2020-08-15 05:19:45 +0000

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,5 @@
 # same as task 0
-exec { 'custom header':
-command  => 'sudo apt-get update &&
-             sudo apt-get -y install nginx && 
-             sudo ufw allow 'Nginx HTTP' &&
-             sudo sed -i "16i \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf &&
-             service nginx restart',
+exec {'header':
+command  => 'sudo apt-get update -y; sudo apt-get install nginx -y; sudo sed -i "15i \\\n\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf; service nginx restart',
 provider => shell,
 }
