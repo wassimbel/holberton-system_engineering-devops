6687772ca070298cdb1189146ea662295e352881
wassimbel <wassim.belhedi1@gmail.com>
2020-08-15 05:05:35 +0000

    update

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+# same as task 0
+exec { ' custom header ':
+command  => 'sudo apt-get update &&
+            sudo apt-get -y install nginx && 
+            sudo ufw allow 'Nginx HTTP' &&
+printf %s "server {
+        listen 80;
+        listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+        location /redirect_me {
+                return 301 http://youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}" > /etc/nginx/sites-available/default &&
+service nginx restart',
+provider => 'shell'
+}
