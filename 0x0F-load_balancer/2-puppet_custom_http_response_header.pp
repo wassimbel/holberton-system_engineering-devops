7fc9d58ca43f8c5be63887a4cb036433e2066bd3
wassimbel <wassim.belhedi1@gmail.com>
2020-08-15 05:34:57 +0000

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # same as task 0
 exec { ' custom header ':
-command  => 'sudo apt-get update; sudo apt-get -y install nginx; sudo ufw allow 'Nginx HTTP';
+command  => 'sudo apt-get update; sudo apt-get -y install nginx;
 printf %s "server {
         listen 80;
         listen [::]:80 default_server;
