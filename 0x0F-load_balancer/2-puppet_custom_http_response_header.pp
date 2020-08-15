f431a349e8e6ba4c08f854eaebd99b9e2244d245
wassimbel <wassim.belhedi1@gmail.com>
2020-08-15 05:22:58 +0000

    update

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,15 @@
+# same as task 0
+exec { ' custom header ':
+command  => 'sudo apt-get update;
+            sudo apt-get -y install nginx; 
+            sudo ufw allow 'Nginx HTTP';
+printf %s "server {
+        listen 80;
+        listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+}" > /etc/nginx/sites-available/default;
+sudo service nginx restart',
+provider  => 'shell'
+}
