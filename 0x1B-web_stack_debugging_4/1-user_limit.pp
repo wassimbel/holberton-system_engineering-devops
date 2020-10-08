13a3eb57e04bb08bcc73d489a4eb15a3d5e771d8
wassimbel <wassim.belhedi1@gmail.com>
2020-10-08 19:15:35 +0000

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # add user
 exec { ' add user ':
-  command => 'adduser holberton';
+  command => 'adduser holberton; usermod -aG holberton';
   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin',]
 }
