a3751c9987e348268ab4278019c63b1e62642e88
wassimbel <wassim.belhedi1@gmail.com>
2020-10-08 18:37:32 +0000

    update

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# fix ULIMIT
+exec {' fix ULIMIT ':
+command  => 'sed -i \'s/ULIMIT=.*/ULIMIT="-n 2000"/g\' /etc/defaukt/nginx; service nginx restart,
+path     => [ '/bin, /sbin, /usr/bin, /usr/sbin' ],
+}
